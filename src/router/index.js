db7cda614b3ae5af7c0c050ff7f41be77ff23a62
xiongyongfeng <2772552493@qq.con>
2025-04-23 11:28:49 +0800

    1.更新
    Signed-off-by: xiongyongfeng <2772552493@qq.con>

--- src/router/index.js
+++ src/router/index.js
@@ -4,26 +4,27 @@ import VueRouter from 'vue-router'
 Vue.use(VueRouter)
 
 const routes = [
-//   {
-//     path: '/esh',
-//     name: 'esh',
-//     component: () => import('@/views/esh.vue'),
-//     meta: {
-//       rank: 3,
-//     },
-//   },
-  {
-    path: '/',
-    name: 'tool',
-    component: () => import('@/views/tool.vue'),
-    meta: {
-      rank: 1,
+    //   {
+    //     path: '/esh',
+    //     name: 'esh',
+    //     component: () => import('@/views/esh.vue'),
+    //     meta: {
+    //       rank: 3,
+    //     },
+    //   },
+    {
+        path: '/',
+        name: 'tool',
+        component: () => import('@/views/tool.vue'),
+        meta: {
+            rank: 1,
+        },
     },
-  },
 ]
 
 const router = new VueRouter({
-  routes,
+    routes,
+    mode: "history",
 })
 
 // router.beforeEach((to, from, next) => {
