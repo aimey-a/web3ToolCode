430e41a915af6962ac51e6ea549f880802db2105
xiongyongfeng <2772552493@qq.con>
2025-04-23 11:08:55 +0800

    1.更新
    Signed-off-by: xiongyongfeng <2772552493@qq.con>

--- src/router/index.js
+++ src/router/index.js
@@ -4,44 +4,12 @@ import VueRouter from 'vue-router'
 Vue.use(VueRouter)
 
 const routes = [
-  {
-    path: '/eths',
-    name: 'eths',
-    component: () => import('@/views/eths.vue'),
-    meta: {
-      rank: 2,
-    },
-  },
-  {
-    path: '/atom',
-    name: 'atom',
-    component: () => import('@/views/atom.vue'),
-    meta: {
-      rank: 3,
-    },
-  },
 //   {
 //     path: '/esh',
 //     name: 'esh',
 //     component: () => import('@/views/esh.vue'),
 //     meta: {
 //       rank: 3,
-//     },
-//   },
-  {
-    path: '/eth',
-    name: 'eth',
-    component: () => import('@/views/eth.vue'),
-    meta: {
-      rank: 4,
-    },
-  },
-//   {
-//     path: '/uni',
-//     name: 'uni',
-//     component: () => import('@/views/uni.vue'),
-//     meta: {
-//       rank: 1,
 //     },
 //   },
   {
